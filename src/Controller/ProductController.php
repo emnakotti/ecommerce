8e15988a1fe9de08d988f6317d5d6e0b6990f568
emnakotti <emna.kotti@esprit.tn>
2024-08-14 15:01:30 +0100

    formulaire

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class ProductController extends AbstractController
+{
+    #[Route('/{slug}', name: 'product_category')]
+    public function category($slug, CategoryRepository $categoryRepository): Response
+    {
+        $category = $categoryRepository->findOneBy(['slug' => $slug]);
+        if (!$category) {
+            throw $this->createNotFoundException("la categorie demandee n' existe pas");
+        }
+
+        return $this->render('product/category.html.twig', [
+            'slug' => $slug,
+            'category' => $category
+        ]);
+    }
+    #[Route('/{category_slug}/{slug}', name: 'product_show')]
+    public function show($slug, ProductRepository $productRepository)
+    {
+
+        $product = $productRepository->findOneBy(['slug' => $slug]);
+        if (!$product) {
+            throw $this->createNotFoundException("le produit demandee n' existe pas");
+        }
+
+        return $this->render('product/show.html.twig', [
+
+            'product' => $product,
+
+        ]);
+    }
+    #[Route("/admin/product/create", name: 'product_create')]
+    public function create(FormFactoryInterface $factory, CategoryRepository $categoryRepository)
+    {
+        $builder = $factory->createBuilder();
+        $builder->add('name', TextType::class, [
+            'label' => 'Nom du produit',
+            'attr' => ['class' => 'form-control', 'placeholder' => 'tapez le nom du produit']
+        ])
+            ->add('shortDescreption', TextareaType::class, [
+                'label' => 'Descreption du peoduit',
+                'attr' => ['class' => 'form-control', 'placeholder' => 'tapez une descreption']
+
+            ])
+            ->add('price', MoneyType::class, [
+                'label' => 'prix du produit',
+                'attr' => ['class' => 'form-control', 'placeholder' => 'tapez le prix du produit']
+            ]);
+        $options = [];
+        foreach ($categoryRepository->findAll() as $category) {
+            $options[$category->getName()] = $category->getId();
+        }
+        $builder->add('category', ChoiceType::class, [
+            'label' => 'Categorie',
+            'attr' => [
+                'class' => 'form-control'
+            ],
+            'placeholder' => '--choisir une categorie--',
+
+            'choices' => $options
+
+        ]);
+
+
+        $form = $builder->getForm();
+        $formView = $form->createView();
+        return $this->render('product/create.html.twig', ['formView' => $formView]);
+    }
+}
